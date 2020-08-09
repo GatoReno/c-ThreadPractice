9f8807f39f51aa72b4453ab4ca9e066dcaf23bb0
OniiOphaim☠👻 <erp.reno@hotmail.com>
2020-08-09 00:49:14 -0500

    commit

---
+++ ThreadSleep/Program.cs
@@ -0,0 +1,81 @@
+﻿using System.Threading;
+using System;
+using System.Net;
+
+namespace ThreadSleep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int num = 1;
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine(num);
+            //    Thread.Sleep(1000);
+            //    num++;
+            //}
+
+            BankAccount acct = new BankAccount(10);
+            Thread[] threads = new Thread[15];
+
+            Thread.CurrentThread.Name = "main";
+
+            for (int i = 0; i < 15; i++)
+            {
+                Thread t = new Thread(new ThreadStart(acct.IssueWithDraw));
+                t.Name = i.ToString();
+                threads[i] = t;               
+            }
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine("Threads {0} Alive : {1} ", threads[i].Name, threads[i].IsAlive);
+
+                threads[i].Start();
+
+                Console.WriteLine("Threads {0} Alive : {1} ", threads[i].Name, threads[i].IsAlive);
+            }
+            Console.WriteLine("Current priority", Thread.CurrentThread.Priority);
+            Console.WriteLine("Thread {0} Ending");
+            Console.ReadLine();
+        }
+    }
+
+    class BankAccount
+    {
+        private Object acctLock = new object();
+        double Balance;
+        public BankAccount(double balance)
+        {
+            Balance = balance;
+        }
+
+        public double WithDraw(double ammount)
+        {
+            if ((Balance - ammount) < 0 )
+            {
+                return Balance;
+            }
+
+            lock (acctLock)
+            {
+                if (Balance >= ammount)
+                {
+                    Console.WriteLine("Removed {0} and {1} lefit in Account"
+                        , ammount, (Balance - ammount));
+
+                    Balance -= ammount;
+                }
+
+                return Balance;
+            }
+        }
+
+         public void IssueWithDraw()
+        {
+            WithDraw(1);
+        }
+
+    }
+}
---
+++ ThreadsRehearsel/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadsRehearsel
+{
+    class Program
+    {
+        //exe multiple pieces of data at the same time
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(Print1);
+            t.Start();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write(0);
+            }
+            Console.ReadLine();
+        }
+
+        static void Print1()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write(1);
+            }
+        }
+    }
+}
