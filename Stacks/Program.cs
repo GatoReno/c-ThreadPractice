fe16f87f51a3b766ea024c6c6e0b2f371d574d13
OniiOphaim☠👻 <erp.reno@hotmail.com>
2020-08-10 12:12:19 -0500

    update stacks

---
+++ Stacks/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+
+namespace Stacks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Stacks!");
+
+            Stack st = new Stack();
+            st.Push(1);
+            st.Push(2);
+            st.Push(3);
+            st.Push(4);
+            foreach (var item in st)
+            {
+                Console.WriteLine($"stack : {item}");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
