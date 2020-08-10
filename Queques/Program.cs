516305fb9adfa550e458031c93dbc7b58eb67d13
OniiOphaim☠👻 <erp.reno@hotmail.com>
2020-08-10 12:03:02 -0500

    update queue part1

---
+++ Queques/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Queques
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Queue : es cola en spanish mr madafawker
+            Console.WriteLine("Queues! \n");
+
+            Queue queque = new Queue();
+            //instead of Add
+            queque.Enqueue(1);
+            queque.Enqueue(2);
+            queque.Enqueue(1);
+            queque.Enqueue(5);
+            queque.Enqueue(6);
+
+            Console.WriteLine("1 in the queue : {0}", queque.Contains(1));
+            Console.WriteLine("remove from queue : {0} remove first item ", queque.Dequeue());
+            Console.WriteLine("peek from queue : {0}", queque.Peek());
+
+            object[] numArray = queque.ToArray();
+            Console.WriteLine(string.Join(", ", numArray));
+            //queque.Clear();
+
+            foreach (var item in queque)
+            {
+                Console.WriteLine($"Queue :  {item}");
+            }
+
+        }
+    }
+}
